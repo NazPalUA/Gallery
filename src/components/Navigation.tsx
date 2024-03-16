115b0a474f59bc4606bf033fb644543776a772f2
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-17 00:07:37 +0200

    Destructure Navbar by adding Dropdown, Navigation, and SearchForm components

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+export default function Navigation() {
+	return (
+		<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+			{/* remove all links except HOME */}
+			<li className="nav-item">
+				<a className="nav-link active" aria-current="page" href="#">
+					Home
+				</a>
+			</li>
+		</ul>
+	)
+}
