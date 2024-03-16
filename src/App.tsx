ddf848653a6ce07fdfd0bd4eef31c3184dfebd12
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-17 01:39:12 +0200

    Add UploadForm component and implement image upload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,74 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import Card from "./components/Card"
 import Navbar from "./components/Navbar"
+import UploadForm from "./components/UploadForm"
+
+type Input = {
+	title: string | null
+	file: File | null
+	path: string | null
+}
+
+const photos = [
+	"https://picsum.photos/id/1001/200/200",
+	"https://picsum.photos/id/1002/200/200",
+	"https://picsum.photos/id/1003/200/200",
+	"https://picsum.photos/id/1004/200/200",
+	"https://picsum.photos/id/1005/200/200",
+	"https://picsum.photos/id/1006/200/200",
+]
 
 function App() {
-	const photos = [
-		"https://picsum.photos/id/1001/200/200",
-		"https://picsum.photos/id/1002/200/200",
-		"https://picsum.photos/id/1003/200/200",
-		"https://picsum.photos/id/1004/200/200",
-		"https://picsum.photos/id/1005/200/200",
-		"https://picsum.photos/id/1006/200/200",
-	]
-
+	const [count, setCount] = useState("")
+	const [inputs, setInputs] = useState<Input>({
+		title: null,
+		file: null,
+		path: null,
+	})
 	const [items, setItems] = useState(photos)
 	const [isCollapsed, collapse] = useState(false)
 	const toggle = () => {
 		collapse(!isCollapsed)
 	}
+	const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.name === "file") {
+			const file = e.target.files?.[0]
+			const path = file ? URL.createObjectURL(file) : null
+			setInputs({
+				...inputs,
+				file: file || null,
+				path: path,
+			})
+		} else if (e.target.name === "title") {
+			setInputs({ ...inputs, title: e.target.value })
+		}
+	}
+	const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (inputs.path !== null) {
+			setItems([inputs.path, ...items])
+		}
+	}
+
+	useEffect(() => {
+		setCount(`you have ${items.length} image${items.length > 1 ? "s" : ""}`)
+	}, [items])
 
 	return (
 		<>
 			<Navbar />
 			<div className="container text-center mt-5">
-				<button
-					className="btn btn-warning mx-2"
-					onClick={() =>
-						setItems(prevItems => [
-							"https://picsum.photos/id/1009/200/200",
-							...prevItems,
-						])
-					}
-				>
-					+Add
+				<button className="btn btn-success float-end" onClick={toggle}>
+					{isCollapsed ? "Close" : "+ Add"}
 				</button>
-				<button className="btn btn-success" onClick={toggle}>
-					collapse
-				</button>
+				<div className="clearfix mb-4"></div>
+				<UploadForm
+					isVisible={isCollapsed}
+					onChange={handleOnChange}
+					onSubmit={handleOnSubmit}
+				/>
+				<h2>{count}</h2>
 				<h1>Gallery</h1>
 				<div className="row">
 					{items.map((photo, index) => (
