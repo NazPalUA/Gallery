972e29db2b9517ab126a1362de44716988deea0e
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-21 22:02:41 +0200

    Add Firestore handler with  writeDoc method

---
+++ src/handlers/firestore.ts
@@ -0,0 +1,30 @@
+import { doc, serverTimestamp, setDoc } from "firebase/firestore"
+import { db } from "../lib/firebase.config"
+
+type Inputs = {
+	title: string | null
+	path: string | null
+}
+
+const Firestore = {
+	writeDoc: async (
+		inputs: Inputs,
+		collection_name: string
+	): Promise<string> => {
+		const randomIndex = Math.floor(Math.random() * 1000000)
+		try {
+			const docRef = doc(db, collection_name, `${randomIndex}`)
+			await setDoc(docRef, {
+				title: inputs.title,
+				path: inputs.path,
+				createdAt: serverTimestamp,
+			})
+			return "Document successfully written!"
+		} catch (e) {
+			console.error("Error adding document: ", e)
+			throw e
+		}
+	},
+}
+
+export default Firestore
