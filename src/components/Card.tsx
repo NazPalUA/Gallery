3d82cdaa46e47d6a98d2b30533bddda01b051a17
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-22 22:30:14 +0200

    Update Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,35 @@
+import { Timestamp } from "firebase/firestore"
+import { useMemo } from "react"
 import { Item } from "../types"
 
 type CardProps = Item
 
-function Card({ path, title }: CardProps) {
+function Card({ path, title, createdAt }: CardProps) {
+	const timestamp = useMemo(() => {
+		if (!createdAt) return
+		const date = `${new Date((createdAt as Timestamp).seconds * 1000)}`.split(
+			" "
+		)
+		return `${date[1]} ${date[2]}, ${date[3]}`
+	}, [])
+
 	return (
 		<div className="col mb-5">
 			<div className="card" style={{ width: "18rem" }}>
-				{path ? (
-					<img src={path} className="card-img-top" alt={title || "image"} />
-				) : (
-					<div className="card-body">
-						<p className="card-text">No Image</p>
-					</div>
-				)}
+				<div
+					style={{
+						height: "220px",
+						backgroundImage: `url(${path || ""})`,
+						backgroundSize: "cover",
+						backgroundPosition: "center",
+						backgroundRepeat: "no-repeat",
+					}}
+				/>
+				<h5 className="text-center mt-1">{title}</h5>
+				<div className="d-flex justify-content-between p-2">
+					<p>{timestamp}</p>
+					<i>@username</i>
+				</div>
 			</div>
 		</div>
 	)
