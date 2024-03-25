0b64b0477fd6465e88b621b942ac30fc32e23b9f
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-25 19:12:43 +0200

    Remove FirestoreContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import { AuthProvider } from "./context/AuthContext.tsx"
-import { Provider as FirestoreProvider } from "./context/FirestoreContext.tsx"
 import "./index.css"
 
 const queryClient = new QueryClient()
@@ -11,11 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<AuthProvider>
-			<FirestoreProvider>
-				<QueryClientProvider client={queryClient}>
-					<App />
-				</QueryClientProvider>
-			</FirestoreProvider>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</AuthProvider>
 	</React.StrictMode>
 )
