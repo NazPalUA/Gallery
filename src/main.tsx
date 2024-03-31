1ff0bf97abd0933e3cef981913599e7b874ff0c7
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-03-31 23:52:48 +0300

    Add routing for stocks page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import App from "./App.tsx"
 import "./index.css"
 
 const queryClient = new QueryClient()
 
+function Stocks() {
+	return <h1>Stocks</h1>
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<Router>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/stocks" element={<Stocks />} />
+				</Routes>
+			</Router>
 		</QueryClientProvider>
 	</React.StrictMode>
 )
