2f63bcc9aa06ef5623c0f19724e3760785141126
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-02 22:34:04 +0300

    Add user-specific filtering to MyImages component

--- src/components/MyImages.tsx
+++ src/components/MyImages.tsx
@@ -1,18 +1,23 @@
+import { useGetUserQuery } from "../firebase/authentication/queries"
 import { useGetStocksQuery } from "../firebase/firestore-database/queries"
 import List from "./List"
 import CountMessage from "./UI/CountMessage"
 
 export default function MyImages() {
 	const { data } = useGetStocksQuery()
+	const { data: userData } = useGetUserQuery()
+
+	const filteredData = data?.filter(item => item.userId === userData?.uid)
+
 	return (
 		<>
 			<h1 className="text-center">My Images</h1>
-			{!data ? (
+			{!filteredData ? (
 				<p className="text-center">No images found</p>
 			) : (
 				<>
-					<CountMessage count={data.length} />
-					<List items={data} />
+					<CountMessage count={filteredData.length} />
+					<List items={filteredData} />
 				</>
 			)}
 		</>
