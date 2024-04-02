19cb621aca668b800b740a118a5de60408d9fd9b
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-03 00:20:58 +0300

    Update navigation links in Navigation component

--- src/components/UI/Navigation.tsx
+++ src/components/UI/Navigation.tsx
@@ -1,23 +1,49 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { useGetUserQuery } from "../../firebase/authentication/queries"
 
 export default function Navigation() {
 	const { data: userData } = useGetUserQuery()
+
 	return (
 		<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 			{/* remove all links except HOME */}
 			<li className="nav-item">
-				<Link className="nav-link active" aria-current="page" to="/">
+				<NavLink
+					className={({ isActive }) =>
+						`${isActive ? "nav-link active" : "nav-link"}`
+					}
+					aria-current="page"
+					to="/"
+				>
 					Home
-				</Link>
+				</NavLink>
 			</li>
-			<li className="nav-item">
-				{userData && (
-					<Link className="nav-link active" aria-current="page" to="/my-images">
-						My Images
-					</Link>
-				)}
-			</li>
+			{userData && (
+				<>
+					<li className="nav-item">
+						<NavLink
+							className={({ isActive }) =>
+								`${isActive ? "nav-link active" : "nav-link"}`
+							}
+							aria-current="page"
+							to="/my-images"
+						>
+							My Images
+						</NavLink>
+					</li>
+					<li className="nav-item">
+						<NavLink
+							className={({ isActive }) =>
+								`${isActive ? "nav-link active" : "nav-link"}`
+							}
+							aria-current="page"
+							to="/profile"
+						>
+							Profile
+						</NavLink>
+					</li>
+				</>
+			)}
 		</ul>
 	)
 }
