b5ca83dbf1887e6e25c01905d0a8522ce7795e79
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-02 22:45:00 +0300

    Add AppRoutes component and update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import App from "./App.tsx"
+import { BrowserRouter as Router } from "react-router-dom"
+import AppRoutes from "./components/AppRoutes.tsx"
 import Layout from "./components/Layout.tsx"
-import Stocks from "./components/MyImages.tsx"
 import "./index.css"
 
 const queryClient = new QueryClient()
@@ -14,10 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 		<QueryClientProvider client={queryClient}>
 			<Router>
 				<Layout>
-					<Routes>
-						<Route path="/" element={<App />} />
-						<Route path="/my-images" element={<Stocks />} />
-					</Routes>
+					<AppRoutes />
 				</Layout>
 			</Router>
 		</QueryClientProvider>
