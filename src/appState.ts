24411186fed3a5c79bc66ee9bb24f866dc3f930d
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-06 23:45:00 +0300

    Refactor appState module to remove fileName property

--- src/appState.ts
+++ src/appState.ts
@@ -3,10 +3,8 @@ import { create } from "zustand"
 type State = {
 	isUploadFormCollapsed: boolean
 	previewUrl: string | null
-	fileName: string | null
 	searchText: string | null
 	setSearchText: (text: string | null) => void
-	setFileName: (name: string | null) => void
 	setPreviewUrl: (url: string | null) => void
 	setIsUploadFormCollapsed: (bool: boolean) => void
 }
@@ -17,8 +15,6 @@ const useAppState = create<State>(set => ({
 	searchText: null,
 	setSearchText: (text: string | null) => set({ searchText: text }),
 	setPreviewUrl: (url: string | null) => set({ previewUrl: url }),
-	fileName: null,
-	setFileName: (name: string | null) => set({ fileName: name }),
 	setIsUploadFormCollapsed: bool =>
 		set(state => ({ ...state, isUploadFormCollapsed: bool })),
 }))
