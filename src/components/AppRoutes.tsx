384d60e8b60e8ede9b9ce3a6cfc22585b4e96d46
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-08 15:16:16 +0300

    Refactor routing in AppRoutes component and add RequireAuth component

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,20 +1,33 @@
 import { Route, Routes } from "react-router-dom"
-import { useGetUserQuery } from "../firebase/authentication/queries"
 import HomePage from "../pages/HomePage"
 import MyImagesPage from "../pages/MyImagesPage"
 import NotFoundPage from "../pages/NotFoundPage"
 import ProfilePage from "../pages/ProfilePage"
 import SingleImagePage from "../pages/SingleImagePage"
+import RequireAuth from "./RequireAuth"
 
 export default function AppRoutes() {
-	const { data: userData } = useGetUserQuery()
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			<Route path="/images/:stockId" element={<SingleImagePage />} />
 			<Route path="*" element={<NotFoundPage />} />
-			{userData && <Route path="/my-images" element={<MyImagesPage />} />}
-			{userData && <Route path="/profile" element={<ProfilePage />} />}
+			<Route
+				path="/my-images"
+				element={
+					<RequireAuth>
+						<MyImagesPage />
+					</RequireAuth>
+				}
+			/>
+			<Route
+				path="/profile"
+				element={
+					<RequireAuth>
+						<ProfilePage />
+					</RequireAuth>
+				}
+			/>
 		</Routes>
 	)
 }
