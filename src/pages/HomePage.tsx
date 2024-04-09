89b00ba52df7598e57d00b6af3582f4864863017
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-09 15:54:22 +0300

    add loading component to HomePage and MyImagesPage components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,32 @@
 import useAppState from "../appState"
 import List from "../components/List"
+import LoadingSpinner from "../components/LoadingSpinner"
 import CountMessage from "../components/UI/CountMessage"
 import UploadForm from "../components/UploadForm"
 import { useGetStocksQuery } from "../firebase/firestore-database/queries"
 
 function HomePage() {
-	const { searchText } = useAppState()
-	const { data } = useGetStocksQuery(searchText || undefined)
+  const { searchText } = useAppState()
+  const { data, isLoading } = useGetStocksQuery(searchText || undefined)
 
-	return (
-		<>
-			<UploadForm />
-			<h1 className="text-center">Gallery</h1>
-			{!data ? (
-				<p className="text-center">No images found</p>
-			) : (
-				<>
-					<CountMessage count={data.length} />
-					<List items={data} />
-				</>
-			)}
-		</>
-	)
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+
+  return (
+    <>
+      <UploadForm />
+      <h1 className="text-center">Gallery</h1>
+      {!data ? (
+        <p className="text-center">No images found</p>
+      ) : (
+        <>
+          <CountMessage count={data.length} />
+          <List items={data} />
+        </>
+      )}
+    </>
+  )
 }
 
 export default HomePage
--- src/pages/MyImagesPage.tsx
+++ src/pages/MyImagesPage.tsx
@@ -1,29 +1,34 @@
 import useAppState from "../appState"
 import List from "../components/List"
+import LoadingSpinner from "../components/LoadingSpinner"
 import CountMessage from "../components/UI/CountMessage"
 import UploadForm from "../components/UploadForm"
 import { useGetUserQuery } from "../firebase/authentication/queries"
 import { useGetStocksQuery } from "../firebase/firestore-database/queries"
 
 export default function MyImagesPage() {
-	const { searchText } = useAppState()
-	const { data } = useGetStocksQuery(searchText || undefined)
-	const { data: userData } = useGetUserQuery()
+  const { searchText } = useAppState()
+  const { data } = useGetStocksQuery(searchText || undefined)
+  const { data: userData, isLoading } = useGetUserQuery()
 
-	const filteredData = data?.filter(item => item.userId === userData?.uid)
+  const filteredData = data?.filter((item) => item.userId === userData?.uid)
 
-	return (
-		<>
-			<UploadForm />
-			<h1 className="text-center">My Images</h1>
-			{!filteredData ? (
-				<p className="text-center">No images found</p>
-			) : (
-				<>
-					<CountMessage count={filteredData.length} />
-					<List items={filteredData} />
-				</>
-			)}
-		</>
-	)
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+
+  return (
+    <>
+      <UploadForm />
+      <h1 className="text-center">My Images</h1>
+      {!filteredData ? (
+        <p className="text-center">No images found</p>
+      ) : (
+        <>
+          <CountMessage count={filteredData.length} />
+          <List items={filteredData} />
+        </>
+      )}
+    </>
+  )
 }
