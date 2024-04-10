973eae6a43c96825aee0fc034a09cf866bd6a6a1
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-10 12:09:00 +0300

    Refactor Preview component to use react-bootstrap Image component

--- src/components/UI/Preview.tsx
+++ src/components/UI/Preview.tsx
@@ -1,16 +1,12 @@
-type PreviewProps = { path: string | null }
-
+import { Image } from "react-bootstrap"
 import style from "./Preview.module.scss"
 
+type PreviewProps = { path: string | null }
+
 export default function Preview({ path }: PreviewProps) {
-	return (
-		path && (
-			<div
-				className={`rounded p-1 m-5 ${style.preview}`}
-				style={{
-					backgroundImage: `url(${path}`,
-				}}
-			></div>
-		)
-	)
+  return (
+    path && (
+      <Image src={path} className={`rounded p-1 m-5 ${style.preview}`} fluid />
+    )
+  )
 }
