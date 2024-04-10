568742ca7d648aab6e6c520add3860382c5060b4
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-10 12:05:01 +0300

    Refactor UploadFormToggleButton component to use react-bootstrap Button component

--- src/components/UploadFormToggleButton.tsx
+++ src/components/UploadFormToggleButton.tsx
@@ -1,18 +1,20 @@
+import { Button } from "react-bootstrap"
 import useAppState from "../appState"
 
 export default function UploadFormToggleButton() {
-	const { setIsUploadFormCollapsed, isUploadFormCollapsed } = useAppState()
+  const { setIsUploadFormCollapsed, isUploadFormCollapsed } = useAppState()
 
-	const toggle = (bool: boolean) => setIsUploadFormCollapsed(bool)
-	return (
-		<>
-			<button
-				className="btn btn-success float-end"
-				onClick={() => toggle(!isUploadFormCollapsed)}
-			>
-				{isUploadFormCollapsed ? "Add" : "+ Close"}
-			</button>
-			<div className="clearfix mb-4" />
-		</>
-	)
+  const toggle = (bool: boolean) => setIsUploadFormCollapsed(bool)
+  return (
+    <>
+      <Button
+        variant="success"
+        className="float-end"
+        onClick={() => toggle(!isUploadFormCollapsed)}
+      >
+        {isUploadFormCollapsed ? "Add" : "+ Close"}
+      </Button>
+      <div className="clearfix mb-4" />
+    </>
+  )
 }
