d83a05f6dd88ae428872780636a0a97bc7bbb2d3
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-10 12:05:09 +0300

    Refactor Avatar component to use react-bootstrap Image component

--- src/components/UI/Avatar.tsx
+++ src/components/UI/Avatar.tsx
@@ -1,15 +1,17 @@
+import { Image } from "react-bootstrap"
+
 type AvatarProps = { url: string | null }
 
 export default function Avatar({ url }: AvatarProps) {
-	return url ? (
-		<img
-			src={url}
-			alt={"profile picture"}
-			className="rounded-circle"
-			width="34"
-			height="34"
-		/>
-	) : (
-		"Login"
-	)
+  return url ? (
+    <Image
+      src={url}
+      alt={"profile picture"}
+      roundedCircle
+      width="34"
+      height="34"
+    />
+  ) : (
+    "Login"
+  )
 }
