a0d7e4d05505c9f6e304c013bd2fca112e7ed90d
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-10 12:04:51 +0300

    Refactor SearchForm component to use react-bootstrap Form, FormControl, and Button components

--- src/components/UI/SearchForm.tsx
+++ src/components/UI/SearchForm.tsx
@@ -1,33 +1,34 @@
 import { ChangeEvent, FormEvent } from "react"
+import { Button, Form, FormControl } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 import useAppState from "../../appState"
 
 export default function SearchForm() {
-	const { searchText, setSearchText } = useAppState()
-	const navigate = useNavigate()
+  const { searchText, setSearchText } = useAppState()
+  const navigate = useNavigate()
 
-	const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setSearchText(e.target.value)
-	}
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+  }
 
-	const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		navigate("/")
-	}
+  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    navigate("/")
+  }
 
-	return (
-		<form className="d-flex" onSubmit={handleOnSubmit}>
-			<input
-				onChange={handleOnChange}
-				value={searchText || ""}
-				className="form-control me-2"
-				type="search"
-				placeholder="Search"
-				aria-label="Search"
-			/>
-			<button className="btn btn-outline-success" type="submit">
-				Search
-			</button>
-		</form>
-	)
+  return (
+    <Form className="d-flex" onSubmit={handleOnSubmit}>
+      <FormControl
+        onChange={handleOnChange}
+        value={searchText || ""}
+        className="me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <Button variant="outline-success" type="submit">
+        Search
+      </Button>
+    </Form>
+  )
 }
