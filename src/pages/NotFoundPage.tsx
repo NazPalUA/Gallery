291f2831aaeeb06c97bcfea2471d43ec144910d9
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-12 00:12:28 +0300

    Refactor NotFoundPage component to use react-bootstrap Button component

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,19 +1,19 @@
+import { Button } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 
 export default function NotFoundPage() {
-	const navigate = useNavigate()
+  const navigate = useNavigate()
 
-	return (
-		<>
-			<button className="btn btn-link mb-5" onClick={() => navigate(-1)}>
-				Back
-			</button>
-			<div className="d-flex justify-content-center align-items-center">
-				<h1>
-					Sorry, the page you're looking for doesn't exist or you don't have
-					access to it.
-				</h1>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <Button variant="link" className="mb-5" onClick={() => navigate(-1)}>
+        Back
+      </Button>
+
+      <h1>
+        Sorry, the page you're looking for doesn't exist or you don't have
+        access to it.
+      </h1>
+    </>
+  )
 }
