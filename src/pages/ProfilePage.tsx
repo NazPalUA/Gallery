292d4d244756b479d18d397ed90f0f255a5c25eb
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-12 00:16:34 +0300

    Refactor ProfilePage component to use react-bootstrap Container, Image, and ListGroup components

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,3 +1,4 @@
+import { Container, Image, ListGroup } from "react-bootstrap"
 import { useGetUserQuery } from "../firebase/authentication/queries"
 import { getUsername } from "../utils/getUsername"
 
@@ -8,30 +9,29 @@ export default function ProfilePage() {
     <>
       <h1 className="text-center">Profile</h1>
       <hr style={{ width: "50%", margin: "3rem auto" }} />
-      <div className="d-flex flex-column flex-md-row justify-content-center align-items-center mb-3">
-        <img
-          className=" mb-3 mb-md-0"
-          style={{ borderRadius: "4px " }}
+      <Container className="d-flex flex-column flex-md-row justify-content-center align-items-center mb-3">
+        <Image
+          className="mb-3 mb-md-0 rounded"
           src={currentUser?.photoURL || ""}
           alt={currentUser?.displayName || "User"}
           width="150"
           height="150"
         />
-        <ul className="list-group mx-5">
-          <li className="list-group-item">
+        <ListGroup className="mx-5">
+          <ListGroup.Item>
             <span className="fs-5 text-capitalize">name:</span>{" "}
             {currentUser?.displayName}
-          </li>
-          <li className="list-group-item">
+          </ListGroup.Item>
+          <ListGroup.Item>
             <span className="fs-5 text-capitalize">nickname:</span>{" "}
             {currentUser ? getUsername(currentUser) : "anonymous"}
-          </li>
-          <li className="list-group-item">
+          </ListGroup.Item>
+          <ListGroup.Item>
             <span className="fs-5 text-capitalize">email:</span>{" "}
             {currentUser?.email}{" "}
-          </li>
-        </ul>
-      </div>
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
     </>
   )
 }
