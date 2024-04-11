ae22ed9a6143ad6ea388616930b9a21126769473
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-04-11 23:02:33 +0300

    Refactor Preview component to use styled-components

--- src/components/UI/Preview.tsx
+++ src/components/UI/Preview.tsx
@@ -1,12 +1,23 @@
 import { Image } from "react-bootstrap"
-import style from "./Preview.module.scss"
+import styled from "styled-components"
 
 type PreviewProps = { path: string | null }
 
+const StyledImage = styled(Image)`
+  width: 75%;
+  height: 300px;
+  background-size: cover;
+
+  @media (min-width: 576px) {
+    width: 30%;
+    height: 200px;
+  }
+
+  @media (min-width: 768px) {
+    height: 300px;
+  }
+`
+
 export default function Preview({ path }: PreviewProps) {
-  return (
-    path && (
-      <Image src={path} className={`rounded p-1 m-5 ${style.preview}`} fluid />
-    )
-  )
+  return path && <StyledImage src={path} className="rounded p-1 m-5" fluid />
 }
